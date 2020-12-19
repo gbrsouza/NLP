a94f9ab21351b4592725fb4f11c0ed594622fc81
gabriel <gabriel_feg@hotmail.com>
2020-12-19 11:43:23 -0300

    projeto final

--- module2/src/classifier.py
+++ module2/src/classifier.py
@@ -1,6 +1,10 @@
 import random
+from statistics import stdev
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
@@ -10,7 +14,6 @@ from sklearn.metrics import recall_score
 
 from module2.src.dataset_reader import read_dataset as reader
 from module2.src.preprocessor import Preprocessor
-import xgboost as xgb
 
 
 def dataset_to_list(dataset):
@@ -38,37 +41,54 @@ def dataset_to_list(dataset):
 
 
 if __name__ == '__main__':
+    print("[1] loading dataset...", end=" ")
     data = reader()
-    X, Y = dataset_to_list(data)
+    print("[Done]")
 
-    pos =0
-    neg = 0
-    for i in Y:
-        if i == 0:
-            neg +=1
-        else:
-            pos +=1
-    print('neg', neg)
-    print('pos', pos)
+    roc_list = []
+    acc_list = []
+    pre_list = []
+    rcl_list = []
 
+    total_executions = 10
+    print("[2] Starting test...")
+    print("[2] total executions:", total_executions)
+    for i in range(total_executions):
+        print("Running step", i, "...")
 
-    # create converter of text to numbers
-    converter = TfidfVectorizer(max_features=10000)
-    X = converter.fit_transform(X).toarray()  # transform text
+        X, Y = dataset_to_list(data)
 
-    # create training and testing vars
-    X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
+        # create converter of text to numbers
+        converter = TfidfVectorizer(max_features=10000)
+        X = converter.fit_transform(X).toarray()  # transform text
 
-    model = RandomForestClassifier(n_estimators=100, bootstrap=True, max_features="sqrt")
-    model.fit(X_train, y_train)
+        # create training and testing vars
+        X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2)
 
-    # Actual class predictions
-    rf_predictions = model.predict(X_test)
+        #model = RandomForestClassifier(n_estimators=100, bootstrap=True, max_features="sqrt")
+        #model = LinearSVC()
+        model = LogisticRegression()
+        model.fit(X_train, y_train)
 
-    # Calculate roc auc
-    print('roc', roc_auc_score(y_test, rf_predictions))
-    print('acc', accuracy_score(y_test, rf_predictions))
-    print('pre', precision_score(y_test, rf_predictions))
-    print('rcl', recall_score(y_test, rf_predictions))
-    # classifier = xgb.train(_params, d_train, num_round)
-    # classifier.predict(d_test)
+        # Actual class predictions
+        rf_predictions = model.predict(X_test)
+
+        roc_list.append(roc_auc_score(y_test, rf_predictions))
+        acc_list.append(accuracy_score(y_test, rf_predictions))
+        pre_list.append(precision_score(y_test, rf_predictions))
+        rcl_list.append(recall_score(y_test, rf_predictions))
+        print("[Done]")
+
+    print("acc:", min(acc_list), "&", max(acc_list), "&", sum(acc_list)/len(acc_list), "&", stdev(acc_list), "\n")
+    print("acc:", min(pre_list), "&", max(pre_list), "&", sum(pre_list) / len(pre_list), "&", stdev(pre_list), "\n")
+    print("acc:", min(rcl_list), "&", max(rcl_list), "&", sum(rcl_list) / len(rcl_list), "&", stdev(rcl_list), "\n")
+
+    print("min pre:", min(pre_list))
+    print("max pre:", max(pre_list))
+    print("avg pre:", sum(pre_list)/len(pre_list))
+    print("std pre:", stdev(pre_list), "\n")
+
+    print("min rcl:", min(rcl_list))
+    print("max rcl:", max(rcl_list))
+    print("avg rcl:", sum(rcl_list)/len(rcl_list))
+    print("std rcl:", stdev(rcl_list))
--- module2/src/dataset_reader.py
+++ module2/src/dataset_reader.py
@@ -10,7 +10,9 @@ def read_dataset():
         read_csv = csv.reader(csvfile, delimiter=',')
         for row in read_csv:
             tweet = row[len(row) - 1]
-            label = 0 if row[len(row) - 2] == '2' else 1
-
+            if row[len(row) - 2] == '2':
+                label = 0
+            elif row[len(row) - 2] == '1':
+                label = 1
             data.append([label, tweet])
         return data
